2e2db22f289eea2293a0d1976d9696a5488e24b2
Chris Snowden <cdsnowden@gmail.com>
2018-01-04 17:11:04 +0000

    printing results to file

--- bash-speedtest-cli.sh
+++ bash-speedtest-cli.sh
@@ -2,14 +2,19 @@
 
 while true
 do
-
+	
 currentTime=$(date "+%H:%M:%S on %Y-%m-%d")
+BASEDIR=$(dirname "$0")
+
+cd $BASEDIR
 
 echo 'Starting a SpeedTest check at' $currentTime
 
-speedtest-cli --json | python -m json.tool
+results=$(speedtest-cli --json)
 
 echo 'Finished with the test at' $currentTime
+
+echo $results | python -m json.tool >> speedtestresults.txt
 
 sleep 600
 
