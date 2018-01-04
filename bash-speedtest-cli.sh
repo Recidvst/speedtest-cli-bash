26977e3ab8c20cfa76e0f6cdcf52f7b5135dacd6
Chris Snowden <cdsnowden@gmail.com>
2018-01-04 22:06:58 +0000

    init data

--- bash-speedtest-cli.sh
+++ bash-speedtest-cli.sh
@@ -8,13 +8,34 @@ BASEDIR=$(dirname "$0")
 
 cd $BASEDIR
 
+
+
 echo 'Starting a SpeedTest check at' $currentTime
 
 results=$(speedtest-cli --json)
 
-echo 'Finished with the test at' $currentTime
+echo 'Finished with the test'
+
+
+
+echo 'Printing to file..'
 
 echo $results | python -m json.tool >> speedtestresults.txt
+
+echo 'File printed'
+
+
+echo 'Pushing file to Github repo..'
+
+git add .
+git commit -m "Speedtest results updated"
+git push origin
+
+echo 'File pushed'
+
+
+
+echo 'Waiting 10 mins..'
 
 sleep 600
 
